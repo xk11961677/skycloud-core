b36ec1e65b3bd7f5114232f3be0540f559966faa
sky <shen11961677@163.com>
2018-03-28 17:20:03 +0800

    修改公用配置文件

--- skycloud-core-zipkin/docker/start.sh
+++ skycloud-core-zipkin/docker/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+registry=registry.cn-hangzhou.aliyuncs.com
+
+repo=zyk
+
 ms_project_name=ms-zipkin
 
 project_name=skycloud-core-zipkin
@@ -12,11 +16,11 @@ sleep 5;
 docker rm $ms_project_name
 sleep 3;
 
-docker rmi -f registry.cn-hangzhou.aliyuncs.com/zyk/$project_name:$project_version
+docker rmi -f $registry/$repo/$project_name:$project_version
 
 sudo docker login --username=username --password=password registry.cn-hangzhou.aliyuncs.com
 
-docker run --name $ms_project_name -d -p 9411:9411 -e TZ='Asia/Shanghai' -v /usr/share/zoneinfo/Asia/Shanghai:/etc/localtime $registry/$project_name:$project_version
+docker run --name $ms_project_name -d -p 9411:9411 -e TZ='Asia/Shanghai' -v /usr/share/zoneinfo/Asia/Shanghai:/etc/localtime $registry/$repo/$project_name:$project_version
 
 docker rmi $(docker images | grep '<none>' |awk '{print $3}')
 
